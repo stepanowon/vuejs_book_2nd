3e78ae4701dd4ac80a672fa1be93e828bbc1e4f3
Hyungseob Won <stepanowon@hotmail.com>
2018-07-22 19:34:01 +0900

    Update RegionButtons.vue

--- examples/ch11/countryapp/src/components/RegionButtons.vue
+++ examples/ch11/countryapp/src/components/RegionButtons.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <button class="region" v-for="region in regions" :key="region.no"
+        <button class="region" v-for="region in regions" :key="region"
             v-bind:class="isSelected(region)"
             @click="changeRegion({region:region})">
             {{region}}
